271cf89ece8dd1fe2ef6a3bc1c2a5d89a8135cf4
patarapon36 <patarapon.ta@hotmail.com>
2023-04-17 22:04:09 +0700

    aftertest restAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 import FrontPage from './components/FrontPage/FrontPage.js';
+interface DataItem {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  thumbnailUrl:any;
+}
 function App() {
+  const [data, setData] = useState<DataItem[] | null>(null);
+  const fetchData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const convertData: DataItem[] = await res.json();
+    console.log(convertData);
+    setData(convertData);
+    
+  };
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
   return (
     <div className="App">
-     <FrontPage/>
+      <FrontPage />
+      { data&&data.map((val) => (
+        <div key={val.id}>
+          <h3>{val.title}</h3>
+          <img src={val.thumbnailUrl} alt={val.title} />
+        </div>
+      ))}
     </div>
   );
 }
