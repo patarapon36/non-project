59afa4648a34594ff5837c02d074515da20d7616
patarapon36 <patarapon.ta@hotmail.com>
2023-04-18 08:45:39 +0700

    brfore pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import FrontPage from './components/FrontPage/FrontPage.js';
+import CallToActionWithVideo from './components/With background image and gradient/CallToActionWithVideo'
 function App() {
   return (
     <div className="App">
      <FrontPage/>
+     <CallToActionWithVideo/>
     </div>
   );
 }
